891cd068164700f10cbb22c8b3c7237017d8c2ef
olek-lytvynenko <olek.lytvynenko@gmail.com>
2023-03-28 15:33:04 +0300

    fix error gulp-gh-page add ruls

--- gulp/tasks/gitHubPage.js
+++ gulp/tasks/gitHubPage.js
@@ -1,7 +1,5 @@
 import ghPages from "gulp-gh-pages";
 
 export const gitHubPage = () => {
-  return global.app.gulp
-    .src(`${global.app.path.buildFolder}/**/*`, {})
-    .pipe(ghPages());
+  return global.app.gulp.src(`./dist/**/*`).pipe(ghPages());
 };
--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -1,9 +1,10 @@
+// eslint-disable-next-line no-unused-vars
 export const server = (done) => {
   app.plugins.browsersync.init({
     server: {
-      baseDir: `${app.path.build.html}`
+      baseDir: `${app.path.build.html}`,
     },
     notify: true,
     port: 5500,
   });
-}
+};
