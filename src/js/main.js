7e8d95b9ed0ee9f04daedb557fe2f0e355c1620a
olek-lytvynenko <olek.lytvynenko@gmail.com>
2023-10-04 22:38:22 +0300

    ver 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,3 @@
-// import * as flsFunction from "./modules/function.js";
-
-// flsFunction.isWebp();
-
-// import Swiper, { Navigation, Pagination } from "swiper";
-
-// const swiper = new Swiper();
-
 window.addEventListener("hashchange", () => {
   if (window.location.hash === "#menu") {
     document.body.classList.add("page__body--with-menu");
